1a0a07ffdc72e099a0ad149b454f0b694a2fdf84
afterloe <lm6289511@gmail.com>
2018-04-07 20:46:53 +0800

    feat: 驱动式响应

--- src/main/kotlin/cn/awpaas/Launch.kt
+++ src/main/kotlin/cn/awpaas/Launch.kt
@@ -3,10 +3,11 @@ package cn.awpaas
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import java.io.Serializable
 
 @SpringBootApplication
 @EnableDiscoveryClient
-class Launch
+class Launch : Serializable
 
 fun main(args: Array<String>) {
     runApplication<Launch>(*args)
