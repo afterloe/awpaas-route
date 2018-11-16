fa7bcaeb673aa3983d3c1ea60c814146cddb2ae6
afterloe <lm6289511@gmail.com>
2018-11-16 13:23:25 +0800

    feat: 转发服务

--- server/Start-tcp.go
+++ server/Start-tcp.go
@@ -36,17 +36,28 @@ func StartUpTCPServer(addr *string) {
 		}
 	}
 }
+
+// 转发连接
 func forwardConn(conn net.Conn) {
 	buffer := receiveData(conn)
 	defer conn.Close()
 	if 1 < len(buffer) {
 		arr := strings.Split(string(buffer), "\r\n")
 		if 1 < len(arr) {
+			flag, host, context := authentication(arr) // 进行鉴权，并提取信息
 			logger.Info(strings.Join(arr, "\r\n"))
+			if flag {
+				forward([]byte(context), host, conn)
+			}
 		}
 	}
 }
 
+func authentication(arr []string) (bool, string, string) {
+	return false, "", ""
+}
+
+//接收数据统一方法
 func receiveData(conn net.Conn) []byte {
 	var buf bytes.Buffer
 	buffer := make([]byte, 8192)
@@ -58,4 +69,19 @@ func receiveData(conn net.Conn) []byte {
 		}
 	}
 	return buf.Bytes()
+}
+
+// 转发服务
+func forward(data []byte, host string, baseconn net.Conn) {
+	conn, _ := net.Dial("tcp", host)
+	conn.Write(data)
+	//time.Sleep(10 * time.Millisecond)
+	bufferHead := receiveData(conn)
+	//time.Sleep(10 * time.Millisecond)
+	bufferBody := receiveData(conn)
+	var buf bytes.Buffer
+	buf.Write(bufferHead)
+	buf.Write(bufferBody)
+	baseconn.Write(buf.Bytes())
+	conn.Close()
 }
