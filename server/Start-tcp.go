79d5def83708e188d708f6430e57617daa4912f4
afterloe <605728727@qq.com>
2018-11-17 20:49:49 +0800

    feat: 网关实现url转发功能

--- server/Start-tcp.go
+++ server/Start-tcp.go
@@ -10,6 +10,8 @@ import (
 	"bytes"
 	"io"
 	"strings"
+	"net/url"
+	// "time"
 )
 
 var notFoundStr, notSupperStr,key string
@@ -33,12 +35,12 @@ func StartUpTCPServer(addr *string, serverCfg map[string]interface{}) {
 		key = "access-token"
 	}
 	netListen, err := net.Listen("tcp", *addr)
+	defer netListen.Close()
 	if nil != err {
 		logger.Error(fmt.Sprintf("can't start server in %s ", *addr))
 		logger.Error(err.Error())
 		os.Exit(100)
 	}
-	defer netListen.Close()
 	logger.Info("waiting request ...")
 	for {
 		conn, err := netListen.Accept()
@@ -51,39 +53,116 @@ func StartUpTCPServer(addr *string, serverCfg map[string]interface{}) {
 	}
 }
 
+func doForwardConn(conn net.Conn) {
+	defer conn.Close()
+	buffer := receiveData(conn)
+	if 1 < len(buffer) {
+		// var method, host, address string
+		// fmt.Sscanf(string(b[:bytes.IndexByte(b[:], '\n')]), "%s%s", &method, &host)
+		arr := strings.Split(string(buffer), "\r\n")
+		add := "127.0.0.1:5984"
+		arr[1] = fmt.Sprintf("Host: %s", add)
+		newStr := strings.Join(arr, "\r\n")
+		server, err := net.Dial("tcp", add)
+		if nil != err {
+			logger.Info(err)
+			return 
+		}
+		// if "CONNECT" == method {
+		// 	fmt.Fprint(conn, "HTTP/1.1 200 Connection established\r\n")
+		// } else {
+		// 	server.Write(b[:n])
+		// }
+		server.Write([]byte(newStr))
+		go io.Copy(server, conn)
+		io.Copy(conn, server)
+		defer server.Close()
+	}
+}
+
 /**
 	请求连接转发工作
 
 	@param: conn - 连接信息
  */
-func doForwardConn(conn net.Conn) {
-	buffer := receiveData(conn)
+func doForwardConnBack(conn net.Conn) {
 	defer conn.Close()
-	if 1 < len(buffer) {
-		arr := strings.Split(string(buffer), "\r\n")
-		if 1 < len(arr) {
-			err, reqInfo := auth(arr) // 提取鉴权信息
-			pathStr, flag := queryWhiteList(reqInfo) // 查询白名单
-			if flag {
-				// 在白名单之内，不需要鉴权即可访问
-				arr[1] = fmt.Sprintf("Host: %s", pathStr)
-				forward([]byte(strings.Join(arr, "\r\n")), pathStr, conn)
-				return
-			}
-			err = linkAndQuery(reqInfo) // 查询鉴权信息
-			err, pathStr = linkAndList(reqInfo) // 查询服务映射表
-			if nil == err {
-				// TODO 转发服务
-				logger.Info(strings.Join(arr, "\r\n"))
-				logger.Info(reqInfo.Token)
-			} else {
-				// TODO 驳回
-				logger.Error(err.Error())
-			}
-		} else {
-			// TODO 驳回
-		}
+	var b [1024]byte
+	n, err := conn.Read(b[:])
+	if nil != err {
+		logger.Info(err)
+		return 
 	}
+	var method, host, address string
+	fmt.Sscanf(string(b[:bytes.IndexByte(b[:], '\n')]), "%s%s", &method, &host)
+	logger.Info(fmt.Sprintf("%s - %s - %s", method, host, address))
+	hostPortURL, err := url.Parse(host)
+
+	logger.Info(hostPortURL)
+	if nil != err {
+		logger.Info(err)
+		return 
+	}
+
+	if hostPortURL.Opaque == "443" { //https访问
+        address = hostPortURL.Scheme + ":443"
+    } else { //http访问
+        if strings.Index(hostPortURL.Host, ":") == -1 { //host不带端口， 默认80
+            address = hostPortURL.Host + ":80"
+        } else {
+            address = hostPortURL.Host
+        }
+	}
+
+	logger.Info(address)
+	
+	server, err := net.Dial("tcp", address)
+	if nil != err {
+		logger.Info(err)
+		return 
+	}
+	if "CONNECT" == method {
+		fmt.Fprint(conn, "HTTP/1.1 200 Connection established\r\n")
+	} else {
+		server.Write(b[:n])
+	}
+	go io.Copy(server, conn)
+	io.Copy(conn, server)
+
+	// buffer := receiveData(conn)
+	// if 1 < len(buffer) {
+	// 	arr := strings.Split(string(buffer), "\r\n")
+	// 	if 1 < len(arr) {
+
+	// 		add := "127.0.0.1:5984"
+	// 		arr[1] = fmt.Sprintf("Host: %s", add)
+	// 		newStr := strings.Join(arr, "\r\n")
+	// 		logger.Info(newStr)
+	// 		forward([]byte(newStr), add, conn)
+
+			// err, reqInfo := auth(arr) // 提取鉴权信息
+			// pathStr, flag := queryWhiteList(reqInfo) // 查询白名单
+			// if flag {
+			// 	// 在白名单之内，不需要鉴权即可访问
+			// 	arr[1] = fmt.Sprintf("Host: %s", pathStr)
+			// 	logger.Info(strings.Join(arr, "\r\n"))
+			// 	forward([]byte(strings.Join(arr, "\r\n")), pathStr, conn)
+			// 	return
+			// }
+			// err = linkAndQuery(reqInfo) // 查询鉴权信息
+			// err, pathStr = linkAndList(reqInfo) // 查询服务映射表
+			// if nil == err {
+			// 	// TODO 转发服务
+			// 	logger.Info(strings.Join(arr, "\r\n"))
+			// 	logger.Info(reqInfo.Token)
+			// } else {
+			// 	// TODO 驳回
+			// 	logger.Error(err.Error())
+			// }
+	// 	} else {
+	// 		// TODO 驳回
+	// 	}
+	// }
 }
 
 /**
@@ -163,9 +242,9 @@ func receiveData(conn net.Conn) []byte {
 func forward(data []byte, host string, baseConn net.Conn) {
 	conn, _ := net.Dial("tcp", host)
 	conn.Write(data)
-	//time.Sleep(10 * time.Millisecond)
+	// time.Sleep(10 * time.Millisecond)
 	bufferHead := receiveData(conn)
-	//time.Sleep(10 * time.Millisecond)
+	// time.Sleep(10 * time.Millisecond)
 	bufferBody := receiveData(conn)
 	var buf bytes.Buffer
 	buf.Write(bufferHead)
