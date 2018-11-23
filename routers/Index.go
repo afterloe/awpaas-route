7e0573061ce35fd621414b779bae557c43c4fcc6
afterloe <lm6289511@gmail.com>
2018-11-23 23:08:45 +0800

    feat: 增加管理节点

--- routers/Index.go
+++ routers/Index.go
@@ -10,6 +10,10 @@ import (
 func Execute(route *gin.RouterGroup) {
 	route.GET("/", Home)
 	route.GET("/whiteList", WhiteList)
+	route.PUT("/whiteList", WhiteListAppend)
+	route.DELETE("/whiteList", WhiteListDel)
 	route.GET("/serviceMap", ServiceMap)
+	route.PUT("/serviceMap", ServiceMapAppend)
+	route.DELETE("/whiteList", ServiceMapDel)
 	route.Any("/tips/:code", Tips)
 }
