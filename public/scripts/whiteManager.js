ffad5b5317e0e9fa143bdc25f07d7ec3788080fa
afterloe <605728727@qq.com>
2018-12-01 00:30:30 +0800

    feat: 服务列表后端服务联调

--- public/scripts/whiteManager.js
+++ public/scripts/whiteManager.js
@@ -147,6 +147,25 @@ const jsonToQueryStr = data => {
     return item.join("&");
 };
 
+const newToRemote = (data, path) => new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.timeout = 15 * 1000;
+    xhr.ontimeout = () => reject(new Error('time is up!'));
+    xhr.open("POST", path);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.send(jsonToQueryStr(data));
+    xhr.onreadystatechange = () => {
+        if (4 === xhr.readyState) {
+            const result = JSON.parse(xhr.responseText);
+            if (200 === xhr.status) {
+                if (200 !== result.code) reject(result.msg);
+                resolve(result.data);
+            } else reject(result.msg);
+        }
+    };
+});
+
 const appendToRemote = (data, path) => new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.timeout = 15 * 1000;
@@ -188,7 +207,7 @@ const getListFromRemote = path => new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.timeout = 15 * 1000;
     xhr.ontimeout = () => reject(new Error('time is up!'));
-    xhr.open("get", path);
+    xhr.open("GET", path);
     xhr.send();
     xhr.onreadystatechange = () => {
         if (4 === xhr.readyState) {
